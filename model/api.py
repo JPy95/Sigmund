d562f16278e3b4b6e9eaf04fd3872de969b163a4
Tubias35 <jpalvees95@gmail.com>
2020-04-10 22:25:38 -0300

    Implementação da API

--- model/api.py
+++ model/api.py
@@ -3,118 +3,123 @@ from flask_restful import Resource, Api
 from datetime import datetime
 from sqlalchemy import create_engine
 from json import dumps
-import ml
 import pandas as pd
+import ml #Algoritmo de Clusterização
 
+
+model = ml.SigmindMl()
 db_connect = create_engine('postgresql://sigmund:Unibh2020@db1-sigmund.cdrfdxumcxao.us-east-1.rds.amazonaws.com:5432/sigmund')
 app = Flask(__name__)
 api = Api(app)
-model = ml.SigmindMl()
-
 
 class Projects(Resource):
-  
-  conn,name,date, = '','',''
+
+  conn,nameProject,date,idProjeto=None,None,None,None
   def __init__(self):
     self.conn = db_connect.connect()
-    self.name = request.json['name']
-    self.date = str(datetime.now())[:str(datetime.now()).find('.')]
-  
+    self.nameProject = request.args.get('nameProject')
+    self.date = request.args.get('date')
+    self.idProjeto = request.args.get('idProjeto')
+
   def post(self):
-    self.conn.execute("insert into sigmundi.projetos values('{0}',DEFAULT, '{1}')".format(self.date,self.name))
+    self.conn.execute("insert into sigmundi.projetos values('{0}',DEFAULT, '{1}')".format(self.date,self.nameProject))
     return self.get()
 
   def get(self):
-    query = self.conn.execute("select * from sigmundi.projetos "
-                "where nomeProjeto = '{0}' and date_trunc('second',datainclusao) = '{1}' ".format(self.name,self.date))
-    result = [dict(zip(tuple(query.keys()), i)) for i in query.cursor]
+    if(self.idProjeto is None):
+      query = self.conn.execute("select * from sigmundi.projetos where nomeProjeto = '{0}' and date_trunc('second',datainclusao) = '{1}' ".format(self.nameProject,self.date))
+      result = [dict(zip(tuple(query.keys()), i)) for i in query.cursor]
+    else:
+      query = self.conn.execute("select * from sigmundi.projetos where idProjeto = {}".format(self.idProjeto))
+      result = [dict(zip(tuple(query.keys()), i)) for i in query.cursor]
+    
     return jsonify(result)
-
 
 class Students(Resource):
 
-  conn,name,email,idProjeto,perfil = '','','','',''
+  conn,nameStudent,email,idProjeto,profile = None,None,None,None,None
+  
   def __init__(self):
     self.conn = db_connect.connect()
-    self.name = request.json['name']
-    self.email = request.json['email']
-    self.idProjeto = request.json['idProjeto']
-    self.perfil = request.json['perfil']
-    
+    self.nameStudent = request.args.get('nameStudent')
+    self.email = request.args.get('email')
+    self.idProjeto = request.args.get('idProjeto')
+    self.perfil = request.args.get('profile')
+
   def post(self):
-    if(self.check_student() == 0):
-      self.conn.execute("insert into sigmundi.alunos values(now(), DEFAULT,'{0}','{1}')".format(self.name,self.email))
-      return self.get()
-    else:
-      return self.get()
+    if(self.check_student(self.email) == 0):
+      self.conn.execute("insert into sigmundi.alunos values(now(), DEFAULT,'{0}','{1}')".format(self.nameStudent,self.email))
+    
+    return self.get()
 
-  def get(self):
-    query = self.conn.execute("select idaluno from sigmundi.alunos where nomeAluno = '{0}' and email = '{1}' ".format(self.name,self.email))
-    result = [dict(zip(tuple(query.keys()), i)) for i in query.cursor]
+  def check_student(self,email):
+    '''
+      Verifica se o aluno já está cadastrado no banco;
+      returno 0 ou retorno > 0;
+    '''
 
-    self.conn.execute("insert into sigmundi.grupos " +
-        "values(now(),{0},null,{1},'{2}','{3}')".format(self.idProjeto, result[0]['idaluno'], self.name, self.perfil))
-
-    return jsonify(result)
-
-  def check_student(self):
     query = self.conn.execute("select * from sigmundi.alunos where email = '{0}' ".format(self.email))
     return query.cursor.rowcount
 
+  def get(self):
+    query = self.conn.execute("select idaluno from sigmundi.alunos where nomeAluno = '{0}' and email = '{1}' ".format(self.nameStudent,self.email))
+    result = [dict(zip(tuple(query.keys()), i)) for i in query.cursor]
+    self.conn.execute("insert into sigmundi.grupos values(now(),{0},null,{1},'{2}','{3}')".format(self.idProjeto, result[0]['idaluno'],self.nameStudent,self.perfil))
+    return jsonify(result)
+
 class Quiz(Resource):
-  conn,ansewrs,idaluno = '','',''
+
+  conn,ansewrs,idaluno = None,None,None
+
   def __init__(self):
     self.conn = db_connect.connect()
-    self.ansewrs = ','.join(list(map(str, request.json['ansewrs'])))
-    self.idaluno = request.json['idaluno']
+    self.respostas = request.args.get('respostas')
+    self.idaluno = request.args.get('idaluno')
 
   def post(self):
-    self.conn.execute('insert into questionarios values(DEFAULT,{0},{1})'.format(self.ansewrs,self.idaluno))
+    self.conn.execute('insert into sigmundi.questionarios values(DEFAULT,{0},{1})'.format(self.respostas,self.idaluno))
+    return dumps({'success':True})
 
 class Groups(Resource):
   
-  conn,idProjeto = '',''
+  conn,idProjeto = None,None
+
   def __init__(self):
     self.conn = db_connect.connect()
-    self.idProjeto = request.json['idProjeto']
-
-  def getDataBaseML(self):
-    query = '''select 
-                a.*
-              from sigmundi.questionarios a 
-              inner join sigmundi.grupos b on b.idaluno = a.idaluno 
-              where b.idprojeto = {}
-            '''.format(self.idProjeto)
-    exect = self.conn.execute(query)
-    result = [dict(zip(tuple(exect.keys()), i)) for i in exect.cursor]
-    return pd.DataFrame(result)
+    self.idProjeto = request.args.get('idProjeto')
 
   def get(self):
-    query = '''select 
-                a.*
-              from sigmundi.questionarios a 
-              inner join sigmundi.grupos b on b.idaluno = a.idaluno 
-              where b.idprojeto = {}
-            '''.format(self.idProjeto)
+    #Busca questionarios
+    query = '''select a.* from sigmundi.questionarios a inner join sigmundi.grupos b on b.idaluno = a.idaluno where b.idprojeto = {}'''.format(self.idProjeto)
     exect = self.conn.execute(query)
-    result = [dict(zip(tuple(exect.keys()), i)) for i in exect.cursor]
-    return jsonify(result)
+
+    #salva base de questionarios em DataFrame
+    base = pd.DataFrame([dict(zip(tuple(exect.keys()), i)) for i in exect.cursor])
+    return self.generateGroup(base)
 
   def buildPuts(self,base):
+    '''
+      Função elabora query de atualização dos dados na tabela Grupos com base a quantidade de registros na base;
+    '''
+    
     query = ''
     for i in range(0,len(base)):
       query+='update sigmundi.grupos set num_grupo = '+str(base['grupos'][i])+' where idprojeto = '+str(self.idProjeto)+' and idaluno = '+str(base['idaluno'][i])+';'
     return query
   
-  def generateGroup(self):
-    base = model.fit(self.getDataBaseML())
-    self.conn.execute(self.buildPuts(base))
-    return self.get()
+  def generateGroup(self,base):
+    '''
+      Função envia base para ordenação de grupos;
+    '''
+    
+    result = model.fit(base)
+    self.conn.execute(self.buildPuts(result))
+    return dumps({'success':True})
 
 api.add_resource(Projects, '/projects')
 api.add_resource(Students, '/students')
-api.add_resource(Quiz, '/quiz') 
-api.add_resource(Groups, '/groups') 
+api.add_resource(Quiz, '/quiz')
+api.add_resource(Groups, '/grupos')
 
 if __name__ == '__main__':
-    app.run()
+    app.run(port='5002')
--- model/ml.py
+++ model/ml.py
@@ -6,16 +6,27 @@ import pickle
 
 class SigmindMl():
   def fit(self, base):
-    
-    X = base.values[:,:-1]
+    X = base.values[:,1:-1]
 
     # Gerando os dados normalizados
     scaler = Normalizer().fit(X)
     normalizedX = scaler.transform(X)
 
     # Carregando o arquivo
-    modeloCluster = pickle.load(open('clustering/ClusteringModel.sav', 'rb'))
+    modeloCluster = pickle.load(open('model/clustering/ClusteringModel.sav', 'rb'))
+    modeloCluster.n_clusters = self.findBestCluster(X)
 
     #Inserindo dados de grupos
     base['grupos'] = modeloCluster.fit(normalizedX).labels_
     return base[['idaluno','grupos']]
+
+  def findBestCluster(self,x):
+    qtdStudent = len(x)
+    qtdGroup = qtdStudent-1
+    
+    while(qtdStudent/qtdGroup < 4):
+      qtdGroup -= 1
+    
+    if(qtdStudent > 13):
+      qtdGroup -= 1
+    return qtdGroup
